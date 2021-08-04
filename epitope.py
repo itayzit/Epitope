18503d3891048e242cfa915e00e655e6bba5faa0
itayzit <itay@hyro.ai>
2021-08-04 21:47:31 +0300

    calculate secondary structure

--- epitope.py
+++ epitope.py
@@ -1,9 +1,11 @@
 import pandas as pd
+import numpy as np
 from quantiprot.metrics import aaindex
 from quantiprot.utils.io import load_fasta_file
 from sklearn.model_selection import train_test_split
 from Bio.PDB import PDBParser
 from Bio.PDB.DSSP import DSSP
+from Bio.SeqUtils.ProtParam import ProteinAnalysis
 
 WINDOW_SIZE = 9
 
@@ -47,3 +49,21 @@ def calculate_rsa():  # draft
     a_key = list(dssp.keys())[2]
     print(dssp[a_key])
 
+
+def calculate_ss(protein, window_size): # not the same window as the one given to the matrix
+    result = []
+    for i in range(len(protein)):
+        start = max(0, i - (window_size // 2))
+        end = min(len(protein), i + window_size // 2)
+        sub_sequence = str(protein[start:end])
+        probabs = list(ProteinAnalysis(sub_sequence).secondary_structure_fraction())
+        probabs.append(1 - sum(probabs))
+        if protein[i].lower() == "p":
+            probabs[0] = 0.0
+        result.append(np.argmax(probabs))
+
+    helix_result = [1 if result[i] == 0 else 0 for i in range(len(protein))]
+    turn_result = [1 if result[i] == 1 else 0 for i in range(len(protein))]
+    sheet_result = [1 if result[i] == 2 else 0 for i in range(protein)]
+    other_structure_result = [1 if result[i] == 3 else 0 for i in range(protein)]
+    return pd.DataFrame(np.vstack((helix_result, turn_result, sheet_result, other_structure_result)))

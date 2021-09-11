3e20069e1bdfed5d551ec28aa5e0c9bac0da943c
Noa Shaham <noash@fb.com>
2021-09-11 12:55:59 +0300

    add command line arguments and write to file

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import argparse
+from typing import Optional
+
 import numpy as np
 import pandas as pd
 import torch
@@ -8,6 +11,13 @@ import epitope
 from sklearn.metrics import roc_curve
 
 OPTIMAL_THRESHOLD = 0.5
+
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument(
+    "-p",
+    dest="protein",
+    required=False,
+)
 
 
 def find_optimal_threshold(net: nn.Module, test_data_loader: data.DataLoader) -> float:
@@ -32,31 +42,40 @@ def find_optimal_threshold(net: nn.Module, test_data_loader: data.DataLoader) ->
     return thresholds[ix]
 
 
-def round_to_threshold(num: float) -> float:
-    if num - OPTIMAL_THRESHOLD >= 0.0:
-        return 1.0
-    return 0.0
-
-
 # TODO: test it!
-def main(protein: str):
+def main(protein: Optional[str]):
     """
     Given a string that represents a protein, return string with uppercase letters where the the network predicts the amino acid to be part of the epitope.
     :param protein: A string that represents a protein
     :return: string with uppercase letters where the epitope is.
     """
+    if not protein:
+        protein = arg_parser.parse_args().protein
     net = torch.load("model")  # TODO
     protein_pred = protein[:4]
     x, _ = epitope.create_dataset(pd.DataFrame(data={"protein": [protein]}))
     dataset = TensorDataset(torch.tensor(x, dtype=torch.float32))
     data_loader = iter(data.DataLoader(dataset, batch_size=1, shuffle=False))
+    all_predictions = []
     with torch.no_grad():
         for i in range(len(data_loader)):
             d = next(data_loader)
-            y = net(d[0]).squeeze(1)
-            prediction = round_to_threshold(y[0])
-            if prediction == 0.0:
-                protein_pred += protein[i + 4].lower()
+            prediction = net(d[0]).squeeze(1)[0]
+            all_predictions.append(prediction)
+            if prediction - OPTIMAL_THRESHOLD >= 0.0:
+                protein_pred += protein[i + 4].upper()
             else:
-                protein += protein[i + 4].upper()
-    return protein_pred + protein[-4:]
+                protein += protein[i + 4].lower()
+    protein_pred += protein[-4:]
+    with open(
+        "output.txt",
+    ) as out:
+        out.write(f"Prediction for protein={protein} is:\n{protein_pred}\n\n")
+        for i in range(len(all_predictions)):
+            out.write(
+                f"probability for acid={protein[i + 4]} is {all_predictions[i]}\n"
+            )
+
+
+if __name__ == "__main__":
+    main(None)

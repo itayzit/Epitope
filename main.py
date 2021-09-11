5f8002771189c542494806f2dc5e735b6029f03c
Noa Shaham <noash@fb.com>
2021-09-11 12:06:42 +0300

    document main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import numpy as np
+import pandas as pd
 import torch
+from torch import nn
 from torch.utils import data
 from torch.utils.data import TensorDataset
 import epitope
@@ -8,14 +10,21 @@ from sklearn.metrics import roc_curve
 OPTIMAL_THRESHOLD = 0.5
 
 
-def find_optimal_threshold(net, test_data_loader):
+def find_optimal_threshold(net: nn.Module, test_data_loader: data.DataLoader) -> float:
+    """
+    Find the optimal threshold according to the roc curve based on the test dataset.
+    :param net: The CNN model.
+    :param test_data_loader:
+    :return: Float of the optimal threshold.
+    """
     all_probs = []
-    with torch.no_grad:
+    with torch.no_grad():
         for d, _ in test_data_loader:
-            y = net(d).squeeze(1)
+            y = net(d.to(torch.cuda)).squeeze(1)
             all_probs.append(y)
     fpr, tpr, thresholds = roc_curve(
-        test_data_loader.dataset.tensors[1], torch.stack(y).flatten()
+        test_data_loader.dataset.tensors[1].cpu().numpy(),
+        torch.cat(all_probs).cpu().numpy(),
     )
     gmeans = np.sqrt(tpr * (1 - fpr))
     ix = np.argmax(gmeans)
@@ -23,28 +32,29 @@ def find_optimal_threshold(net, test_data_loader):
     return thresholds[ix]
 
 
-def round_to_threshold(ten: torch.Tensor):
-    predictions = torch.zeros(ten.shape)
-    for i, num in enumerate(ten):
-        if num - OPTIMAL_THRESHOLD >= 0.0:
-            predictions[i] = 1.0
-        else:
-            predictions[i] = 0.0
-    return predictions
+def round_to_threshold(num: float) -> float:
+    if num - OPTIMAL_THRESHOLD >= 0.0:
+        return 1.0
+    return 0.0
 
 
 # TODO: test it!
 def main(protein: str):
+    """
+    Given a string that represents a protein, return string with uppercase letters where the the network predicts the amino acid to be part of the epitope.
+    :param protein: A string that represents a protein
+    :return: string with uppercase letters where the epitope is.
+    """
     net = torch.load("model")  # TODO
     protein_pred = protein[:4]
-    x, _ = epitope.create_dataset([protein])
+    x, _ = epitope.create_dataset(pd.DataFrame(data={"protein": [protein]}))
     dataset = TensorDataset(torch.tensor(x, dtype=torch.float32))
     data_loader = iter(data.DataLoader(dataset, batch_size=1, shuffle=False))
     with torch.no_grad():
         for i in range(len(data_loader)):
             d = next(data_loader)
             y = net(d[0]).squeeze(1)
-            prediction = round_to_threshold(y)[0]
+            prediction = round_to_threshold(y[0])
             if prediction == 0.0:
                 protein_pred += protein[i + 4].lower()
             else:
